865f56881b0e1286dc7363f27926f64719fa374b
shohjahon <shohjahonobruyev3@gmail.com>
2024-12-21 12:04:56 +0500

    crud

--- root/settings.py
+++ root/settings.py
@@ -54,6 +54,7 @@ INSTALLED_APPS = [
     'drf_yasg',
     'rest_framework',
     'drf_spectacular',
+    'django_filters',
     'rest_framework_simplejwt',
 
     # INSTALLED APPS
@@ -88,6 +89,9 @@ REST_FRAMEWORK = {
         # 'rest_framework_simplejwt.authentication.JWTAuthentication',
         'rest_framework_simplejwt.authentication.JWTStatelessUserAuthentication',
     ),
+    'DEFAULT_FILTER_BACKENDS': (
+            'django_filters.rest_framework.DjangoFilterBackend',
+        ),
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,  # Adjust the page size as needed
