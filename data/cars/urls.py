0f78d19d86ec0c3dcba449312e16f2c12df31254
shohjahon <shohjahonobruyev3@gmail.com>
2024-12-21 14:15:32 +0500

    crud

--- data/cars/urls.py
+++ data/cars/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from .views import *
 urlpatterns = [
-    path('car-create/', CarCreateAPIView.as_view(), name='create'),
-    path('cars-list/', CarsListAPIView.as_view(), name='list'),
-    path('cars-by-id/<uuid:pk>/', CarByIDAPIView.as_view(), name='id'),
-    path('car-update/<uuid:pk>/', CarUpdateAPIView.as_view(), name='update'),
+    path('create/', CarCreateAPIView.as_view(), name='create'),
+    path('list/', CarsListAPIView.as_view(), name='list'),
+    path('list-no-pg/',CarsList_no_pg_APIView.as_view(),name='list-no-pg'),
+    path('by-id/<uuid:pk>/', CarByIDAPIView.as_view(), name='id'),
+    path('update/<uuid:pk>/', CarUpdateAPIView.as_view(), name='update'),
 ]
--- data/cars/views.py
+++ data/cars/views.py
@@ -9,6 +9,7 @@ from rest_framework.generics import (
 )
 from rest_framework.generics import RetrieveAPIView
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 from rest_framework_simplejwt.authentication import JWTAuthentication
 
 from .serializers import *
@@ -19,7 +20,6 @@ class CarCreateAPIView(CreateAPIView):
     queryset = Car.objects.all()
     serializer_class = CarCreateSerializer
     # permission_classes = (IsAuthenticated,)
-
 
 
 class CarsListAPIView(ListAPIView):
@@ -34,11 +34,29 @@ class CarsListAPIView(ListAPIView):
     ordering_fields = ['number']
     search_fields = ['name']
 
+class CarsList_no_pg_APIView(ListAPIView):
+    queryset = Car.objects.all()
+    serializer_class = CarListserializer
+    # permission_classes = (IsAuthenticated,)
+
+    filter_backends = [DjangoFilterBackend, OrderingFilter, SearchFilter]
+    filterset_fields = ['name','number','type_of_payment',
+                        'with_trailer',"fuel_type","price_uzs","price_usd",
+                        'distance_travelled']
+    ordering_fields = ['number']
+    search_fields = ['name']
+
+    def get_paginated_response(self, data):
+        return Response(data)
+
 
 class CarByIDAPIView(RetrieveAPIView):
     queryset = Car.objects.all()
     serializer_class = CarListserializer
     permission_classes = (IsAuthenticated,)
+
+
+
 
     def get_object(self):
         try:
