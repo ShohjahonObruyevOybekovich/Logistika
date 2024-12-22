d25336d5aa0aa9c200e56d2633d05fe50ac8d058
Shukurulloh Komiljonov <komiljonovshukurullokh@gmail.com>
2024-12-22 19:40:00 +0500

    dfg

--- config/data/city/serializers.py
+++ config/data/city/serializers.py
@@ -1,36 +1,17 @@
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
 
-from .models import City,Region
+from .models import City, Region
 
 User = get_user_model()
 
-class CityCreateSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = City
-        fields = [
-            'name','region',
-        ]
-
-
-
-
-class CityListserializer(serializers.ModelSerializer):
-    class Meta:
-        model = City
-        fields = [
-            'name','region',
-        ]
-
-class RegionCreateSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Region
-        fields = [
-            'name',
-        ]
 
 class RegionSerializer(serializers.ModelSerializer):
+    
     class Meta:
         model = Region
-        fields = ['name']
-
+        fields = [
+            "name",
+            "price1",
+            "price2",
+        ]
--- config/data/city/urls.py
+++ config/data/city/urls.py
@@ -1,14 +1,7 @@
 from django.urls import path
 from .views import *
-urlpatterns = [
-    path("create/",CityCreateAPIView.as_view(), name="create"),
-    path("update/<uuid:pk>",CityUpdateAPIView.as_view(), name="update"),
-    path("delete/<uuid:pk>",CityDeleteAPIView.as_view(), name="delete"),
-    path("list/",CityListAPIView.as_view(), name="list"),
-    path('list-no-pg/',RegionListnopgAPIView.as_view(), name='list-no-pg'),
 
-    path("region-create/",RegionCreateAPIView.as_view(), name="region-create"),
-    path("region-update/<uuid:pk>",RegionUpdateAPIView.as_view(), name="region-update"),
-    path("region-delete/<uuid:pk>",RegionDeleteAPIView.as_view(), name="region-delete"),
-    path("region-list/",RegionListAPIView.as_view(), name="region-list"),
+urlpatterns = [
+    path("", RegionListAPIVIew.as_view()),
+    path("<uuid:pk>", RegionDetailAPIView.as_view()),
 ]
