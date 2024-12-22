6ca1e2172ad3b898c8ca103a5157ddfc713bbadc
Shukurulloh Komiljonov <komiljonovshukurullokh@gmail.com>
2024-12-22 19:32:45 +0500

    dfg

--- config/data/gas/signals.py
+++ config/data/gas/signals.py
@@ -1,6 +1,6 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from .models import GasPurchase
+from .models import GasPurchase, GasSale
 
 
 @receiver(post_save, sender=GasPurchase)
@@ -8,4 +8,13 @@ def on_gas_purchased(sender, instance: GasPurchase, created, **kwargs):
     if created:
 
         instance.station.remaining_gas += instance.amount
+        instance.station.save()
+
+
+@receiver(post_save, sender=GasSale)
+def on_gas_sold(sender, instance: GasSale, created, **kwargs):
+
+    if created:
+
+        instance.station.remaining_gas -= instance.amount
         instance.station.save()
