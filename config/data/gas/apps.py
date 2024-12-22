325e2d656e6402b9fc899b6d67de8127e703f04e
Shukurulloh Komiljonov <komiljonovshukurullokh@gmail.com>
2024-12-22 19:16:57 +0500

    dfg

--- config/data/gas/apps.py
+++ config/data/gas/apps.py
@@ -4,6 +4,6 @@ from django.apps import AppConfig
 class GasConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "data.gas"
-    
+
     def ready(self):
         import data.gas.signals
