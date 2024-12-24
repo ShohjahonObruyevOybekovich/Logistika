b12d7679b2c2ff1941cc6791556f373855970e2f
shohjahon <shohjahonobruyev3@gmail.com>
2024-12-24 18:08:11 +0500

    crud

--- config/data/flight/urls.py
+++ config/data/flight/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path("", FlightListAPIView.as_view(), name="flight-list"),
     # path("create/", FlightCreateAPIView.as_view(), name="flight-create"),
     path("<uuid:pk>/", FlightRetrieveAPIView.as_view(), name="flight-retrieve"),
-    path("stats/<uuid:pk>", FlightStatsAPIView.as_view(), name="flight-list"),
-    path("driver/<uuid:id>",FlightHistoryAPIView.as_view(), name="flight-history"),
+    path("stats/<int:pk>", FlightStatsAPIView.as_view(), name="flight-list"),
+    path("driver/<int:id>",FlightHistoryAPIView.as_view(), name="flight-history"),
                ]
