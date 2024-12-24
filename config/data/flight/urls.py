6762a547c2feb764d2e8bd87f1cf23778cb4192c
shohjahon <shohjahonobruyev3@gmail.com>
2024-12-24 16:43:05 +0500

    crud

--- config/data/flight/urls.py
+++ config/data/flight/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path("", FlightListAPIView.as_view(), name="flight-list"),
     # path("create/", FlightCreateAPIView.as_view(), name="flight-create"),
     path("<uuid:pk>/", FlightRetrieveAPIView.as_view(), name="flight-retrieve"),
+    path("stats/<uuid:pk>", FlightStatsAPIView.as_view(), name="flight-list"),
                ]
--- config/data/flight/views.py
+++ config/data/flight/views.py
@@ -1,3 +1,4 @@
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.generics import CreateAPIView, ListCreateAPIView, RetrieveAPIView, RetrieveUpdateDestroyAPIView, \
     ListAPIView
 from rest_framework.permissions import IsAuthenticated
@@ -22,4 +23,27 @@ class FlightRetrieveAPIView(RetrieveUpdateDestroyAPIView):
 #     queryset = Flight.objects.all()
 #     serializer_class = FlightSerializer
 #     permission_classes = [IsAuthenticated]
+import django_filters
+
+
+class FlightFilter(django_filters.FilterSet):
+    car_id = django_filters.UUIDFilter(field_name='car__id', lookup_expr='exact')
+
+    class Meta:
+        model = Flight
+        fields = ['car_id']
+
+class FlightStatsAPIView(ListAPIView):
+    serializer_class = FlightListserializer
+    permission_classes = (IsAuthenticated,)
+    filter_backends = (DjangoFilterBackend,)
+    filterset_class = FlightFilter  # Use the filter class
+
+    def get_queryset(self):
+        # Retrieve the 'pk' (car_id) from the URL kwargs
+        car_id = self.kwargs.get('pk')
+        if car_id:
+            # Filter the queryset by car_id
+            return Flight.objects.filter(car__id=car_id)
+        return Flight.objects.none()
 
