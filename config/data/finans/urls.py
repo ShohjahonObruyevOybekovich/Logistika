f9e86f87dc8525062b2888858ccb0b11a2133dfc
shohjahon <shohjahonobruyev3@gmail.com>
2024-12-25 13:14:15 +0500

    changes price

--- config/data/finans/urls.py
+++ config/data/finans/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path("<uuid:pk>/", FinansDetail.as_view(), name="flight-retrieve"),
     path("finans/driver/<int:pk>",FinansDriver.as_view(), name="flight-driver-detail"),
 
+    path("export-logs/", ExportLogsToExcelAPIView.as_view(), name="export_logs"),
+
                ]
--- config/data/finans/views.py
+++ config/data/finans/views.py
@@ -1,7 +1,15 @@
+from django.views import View
+import openpyxl
+from drf_yasg import openapi
+from drf_yasg.utils import swagger_auto_schema
+from openpyxl.styles import Font
+
+from django.http import HttpResponse
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import OrderingFilter, SearchFilter
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.views import APIView
 
 from data.finans.models import Logs
 from data.finans.serializers import FinansListserializer
@@ -44,3 +52,49 @@ class FinansDriver(ListCreateAPIView):
         if driver_id:
             return Logs.objects.filter(employee__id=driver_id, kind="PAY_SALARY")
         return Logs.objects.none()
+
+
+class ExportLogsToExcelAPIView(APIView):
+    @swagger_auto_schema(
+        manual_parameters=[
+            openapi.Parameter(
+                "action", openapi.IN_QUERY, description="Filter by action (INCOME, OUTCOME)", type=openapi.TYPE_STRING
+            )
+        ],
+        responses={200: "Excel file generated"}
+    )
+    def get(self, request):
+        action_filter = request.GET.get("action", None)
+        if action_filter in ["INCOME", "OUTCOME"]:
+            logs = Logs.objects.filter(action=action_filter).order_by("id")
+        else:
+            logs = Logs.objects.all().order_by("action", "id")
+
+        workbook = openpyxl.Workbook()
+        sheet = workbook.active
+        sheet.title = "Logs"
+        headers = [
+            "Action", "Miqdori (UZS)","Mashina", "Haydovchi" , "Reyis" ,
+            "Turi", "Sababi", "Comment", "Yaratilgan vaqti"
+                   ]
+        for col_num, header in enumerate(headers, 1):
+            cell = sheet.cell(row=1, column=col_num)
+            cell.value = header
+
+        for row_num, log in enumerate(logs, 2):
+            sheet.cell(row=row_num, column=1).value = log.action
+            sheet.cell(row=row_num, column=2).value = log.amount_uzs
+            sheet.cell(row=row_num, column=3).value = log.car.number
+            sheet.cell(row=row_num, column=4).value = log.employee.phone
+            sheet.cell(row=row_num, column=5).value = log.flight.cargo_info
+            sheet.cell(row=row_num, column=6).value = log.kind
+            sheet.cell(row=row_num, column=7).value = log.reason
+            sheet.cell(row=row_num, column=8).value = log.comment
+            sheet.cell(row=row_num, column=9).value = log.created_at.strftime('%d-%m-%Y   %H:%M')
+
+        response = HttpResponse(
+            content_type="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+        )
+        response["Content-Disposition"] = f'attachment; filename="Logs_{action_filter or "All"}.xlsx"'
+        workbook.save(response)
+        return response
