8fbc762d6bdcbe140ccc2a215f5be9fd17a89637
shohjahon <shohjahonobruyev3@gmail.com>
2024-12-27 13:46:18 +0500

    oil purchase edited

--- config/data/flight/signals.py
+++ config/data/flight/signals.py
@@ -8,49 +8,42 @@ from ..finans.models import Logs
 @receiver(post_save, sender=Flight)
 def handle_flight_status_update(sender, instance: Flight, created, **kwargs):
     if created:
-        # if instance.status == "INACTIVE":
-        try:
-            with transaction.atomic():
-                # Log income if applicable
-                if instance.price_uzs > 0:
-                    Logs.objects.create(
-                        action="INCOME",
-                        amount_uzs=instance.price_uzs,
-                        kind="FLIGHT",
-                        comment=f"Доход от рейса с ID {instance.departure_date.date()}.",
-                        flight=instance,
-                        employee=instance.driver,
-                    )
 
-                # Log outcome for driver expenses
-                if instance.driver_expenses_uzs > 0:
-                    Logs.objects.create(
-                        action="OUTCOME",
-                        amount_uzs=instance.driver_expenses_uzs,
-                        kind="FLIGHT",
-                        comment=f"Расход на рейс с ID {instance.departure_date.date()}.",
-                        flight=instance,
-                        employee=instance.driver,
-                    )
-                if instance.other_expences > 0:
-                    Logs.objects.create(
-                        action="OUTCOME",
-                        amount_uzs=instance.other_expences,
-                        kind="FLIGHT",
-                        comment=f"Расход за рейс для водителя {instance.driver.full_name}: {instance.other_expences} ",
-                    )
-                    instance.driver.balance_uzs += instance.other_expences
-                    instance.driver.save()
+        if instance.price_uzs > 0:
+            Logs.objects.create(
+                action="INCOME",
+                amount_uzs=instance.price_uzs,
+                kind="FLIGHT",
+                comment=f"Доход от рейса с ID {instance.departure_date}.",
+                flight=instance,
+                employee=instance.driver,
+            )
+
+        # Log outcome for driver expenses
+        if instance.driver_expenses_uzs > 0:
+            Logs.objects.create(
+                action="OUTCOME",
+                amount_uzs=instance.driver_expenses_uzs,
+                kind="FLIGHT",
+                comment=f"Расход на рейс с ID {instance.departure_date}.",
+                flight=instance,
+                employee=instance.driver,
+            )
+        if instance.other_expences:
+            Logs.objects.create(
+                action="OUTCOME",
+                amount_uzs=instance.other_expences,
+                kind="FLIGHT",
+                comment=f"Расход за рейс для водителя {instance.driver.full_name}: {instance.other_expences} ",
+            )
+            instance.driver.balance_uzs += float(instance.other_expences)
+            instance.driver.save()
 
 
-                # Update driver balance
-                if instance.driver:
-                    instance.driver.balance_uzs += instance.driver_expenses_uzs
-                    instance.driver.save()
-
-        except Exception as e:
-            print(f"Error handling flight status update signal: {e}")
-
+        # Update driver balance
+        if instance.driver:
+            instance.driver.balance_uzs += instance.driver_expenses_uzs
+            instance.driver.save()
 
 
 @receiver(post_save, sender=Ordered)
