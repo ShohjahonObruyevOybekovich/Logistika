b4009a75a228dacf02dbd2e2636f7cf16f203915
shohjahon <shohjahonobruyev3@gmail.com>
2025-01-10 20:04:54 +0500

    oil purchase edited

--- config/data/flight/views.py
+++ config/data/flight/views.py
@@ -291,7 +291,7 @@ class FlightCloseApi(APIView):
                 flight.flight_balance_uzs = float(flight.flight_balance_uzs or 0)
                 flight.flight_expenses_uzs = float(flight.flight_balance_uzs or 0)
 
-                if flight.type == "OUT":
+                if flight.flight_type == "OUT":
                     ic(f"Updating driver balance for {flight.driver.full_name}")
                     driver = flight.driver
 
