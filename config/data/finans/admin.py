9493d83f9ff4edd11e63d2b601a38ea0eeb265ed
shokh_smee <shohjahonobruyev3@gmail.com>
2025-06-19 14:18:58 +0500

    docker edit

--- config/data/finans/admin.py
+++ config/data/finans/admin.py
@@ -5,8 +5,8 @@ from .models import Logs
 # Register your models here.
 
 
-@admin.register(Logs)
-class SaleStudentAdmin(admin.ModelAdmin):
-    list_display = ("employee__full_name","action","amount","car__name", "flight__region__name", "created_at")
-    search_fields = ("employee__full_name","car__name","flight__region__name")
-    list_filter = ("action","kind","amount_type")
+# @admin.register(Logs)
+# class SaleStudentAdmin(admin.ModelAdmin):
+#     list_display = ("employee__full_name","action","amount","car__name", "flight__region__name", "created_at")
+#     search_fields = ("employee__full_name","car__name","flight__region__name")
+#     list_filter = ("action","kind","amount_type")
