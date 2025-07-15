374596f7591e50a7a152949561d669bb0b4fb45e
shokh_smee <shohjahonobruyev3@gmail.com>
2025-07-15 11:39:21 +0500

    docker edit

--- config/employee/admin.py
+++ config/employee/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 from employee.models import Employee
+
+admin.site.unregister(Employee)
+
+@admin.register(Employee)
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display = ("full_name", "phone","balance","balance_price_type",)
+    list_filter = ("balance",)
+    search_fields = ("full_name",)
